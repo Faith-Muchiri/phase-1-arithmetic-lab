5524c3fe56f7734f7d26a5c669980d03f49f9602
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-08-16 12:21:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+const num1 =62;
+const num2 =1;
+const num3 = 24;
+const num4 =5;
+
+const multiply =num1*num2;
+const random = Math.ceil(Math.random());
+const mod = num3 % num4;
+const max =Math.max(2,4,7,18,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
